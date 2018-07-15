0dff43a5799247444ac7f1cbd257a06ffaa86aa1
alexggg99 <agashchuk@corp.local>
2018-07-15 13:59:34 +0300

    init app structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+          <a href="https://www.pluralsight.com/courses/react-redux-react-router-es6">
+            https://www.pluralsight.com/courses/react-redux-react-router-es6
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
--- tools/startMessage.js
+++ tools/startMessage.js
@@ -1,5 +1,5 @@
 import colors from 'colors';
 
-/* eslink-disable no-console */
+/*eslint-disable no-console */
 
-// console.log('start app in dev mode...'.green);
+console.log('Starting app in dev mode...'.green);
